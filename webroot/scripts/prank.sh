f1870ef3d2abf592ed73ad3ea3788002a187aba6
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2018-11-21 17:02:12 +0000

    prank: use msg instead of info

--- webroot/scripts/prank.sh
+++ webroot/scripts/prank.sh
@@ -4,13 +4,14 @@ header "Geminio bootstrap script - Surprise Edition"
 
 emerge mpv
 
-info "Downloading video.mp4"
+msg "Downloading video.mp4"
 /bin/wget --no-verbose "http://geminio.rnl.tecnico.ulisboa.pt/files/video.mp4"
 
 sleep 1
 mpv -vo caca video.mp4
 
 clear
-info "bye bye..."
+msg "bye bye..."
+
 sleep 1
-reboot -f
+/bin/reboot
