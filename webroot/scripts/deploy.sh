c7573a30803a144239652cb35ef9ea0ac8dc5958
Tomás Cunha <tomas.cunha@rnl.tecnico.ulisboa.pt>
2019-09-06 16:53:58 +0100

    Add dynamic completeScript download

--- webroot/scripts/deploy.sh
+++ webroot/scripts/deploy.sh
@@ -10,6 +10,8 @@ info "Downloading torrent file"
 /bin/wget --no-verbose "http://dolly.rnl.tecnico.ulisboa.pt/deploy.torrent" -P /torrents/
 
 sleep 5
+msg "Downloading complete script"
+/bin/wget -O /completeScript.sh "http://bootstrap.rnl.tecnico.ulisboa.pt/completeScript.sh"
 
 info "Starting Transmission"
 transmission-daemon --logfile /transmission.log --config-dir /var/lib/transmission
